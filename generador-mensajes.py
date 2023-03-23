702ae2ce38dbc26a2e0652c23cf03ca0ca4c1a4e
nklopstock611 <n.klopstock@uniandes.edu.co>
2023-03-23 17:49:06 -0500

    generador archivos

--- generador-mensajes.py
+++ generador-mensajes.py
@@ -1,15 +1,8 @@
 
-<<<<<<< HEAD
-# 1MB para UDP
-
-file = open("mensajes/1MB.txt", "w")
-texto = 'a' * (100 * 1024 * 10)
-=======
 #1MB para UDP
 
 file = open("mensajes/1MB.txt", "w")
 texto = 'a' * (250 * 1024 * 10)
->>>>>>> 71b75a39e34284fe3266a26c6e508cb73cd95020
 file.write(texto)
 file.close()
 
