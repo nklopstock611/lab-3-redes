c55d9793b0aeb64d4a0171133ab1037ea1377650
Nicolás Klopstock Triana <77993013+nklopstock611@users.noreply.github.com>
2023-03-22 20:28:06 -0500

    Create generador-mensajes.py

---
+++ generador-mensajes.py
@@ -0,0 +1,14 @@
+
+# 100MB para UDP
+
+file = open("mensajes/100MB.txt", "w")
+texto = 'a' * (100 * 1024 * 1000)
+file.write(texto)
+file.close()
+
+# 250MB para UDP
+
+file = open("mensajes/250MB.txt", "w")
+texto = 'a' * (250 * 1024 * 1000)
+file.write(texto)
+file.close()
