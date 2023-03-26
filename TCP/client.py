65c94c56830ddf1c944993cf3d68a685e269e9e6
Brian <b.riverah@uniandes.edu.co>
2023-03-26 16:17:01 -0500

    actualizacion servidor TCP

---
+++ TCP/client.py
@@ -0,0 +1,94 @@
+import socket
+import threading
+import os 
+import hashlib
+
+IP = "192.168.20.57"
+# IP = socket.gethostbyname(socket.gethostname())
+PORT = 5566
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!END"
+hash_incorrecto=0
+
+def receive_messages(client_socket,filename,filesize,id_cliente,num_clients):
+    print(f"[CLIENT] Waiting for messages...")
+    listo= input("Ingrese READY cuando este listo para recibir el archivo: ")
+    client_socket.sendall("READY".encode(FORMAT))
+    print(f"[CLIENT] ENVIO DE READY")
+    print (f"[CLIENT] Esperando archivo {filename}")
+    # verify that dir exists
+    if not os.path.exists('ArchivosRecibidos'):
+        os.makedirs('ArchivosRecibidos')
+
+    with open(f"ArchivosRecibidos/Cliente{id_cliente}-Prueba-{num_clients}.txt", 'wb') as f:
+        offset = 0
+        while offset < int(filesize):
+            # Leer el archivo en bloques de 1024 bytes
+            data = client_socket.recv(SIZE)
+            # Enviar el bloque al cliente
+            f.write(data)
+            offset += len(data)
+
+    client_socket.sendall("FIN".encode(FORMAT))
+            
+    
+    print(f"[CLIENT] Archivo recibido")
+
+    archivo = open(f"ArchivosRecibidos/Cliente{id_cliente}-Prueba-{num_clients}.txt", 'r')
+    HASH = client_socket.recv(SIZE).decode(FORMAT)
+    HASH_CALCULADO = hashlib.md5(archivo.read().encode()).hexdigest()
+
+    print(f"[CLIENT] HASH recibido: {HASH}")
+    print (f"[CLIENT] HASH calculado: {HASH_CALCULADO}")
+    correcto_=""
+    if HASH == HASH_CALCULADO:
+        print(f"[CLIENT] HASH correcto")
+        correcto_="correcto"
+    else:
+        print(f"[CLIENT] HASH incorrecto")
+        global hash_incorrecto
+        hash_incorrecto+=1
+        correcto_="incorrecto"
+    client_socket.sendall(correcto_.encode(FORMAT))
+
+    client_socket.close()
+
+def main():
+    # Conexión al servidor
+    client_sockets = []
+    num_clients = int(input("Ingrese el número de clientes: "))
+    client_socket_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket_.connect(ADDR)
+    print(f"[KING CLIENT] Connected to server at {IP}:{PORT}")
+    client_socket_.sendall(str(num_clients).encode(FORMAT))
+    archivo_transmision = input("Ingrese el nombre del archivo a transmitir: ")
+    print(f"[KING CLIENT] se espera el archivo {archivo_transmision}")
+    archivo_transmision = archivo_transmision.encode(FORMAT)
+    client_socket_.sendall(archivo_transmision)
+    print(f"[KING CLIENT] se envio el nombre del archivo {archivo_transmision}")
+    filesize = client_socket_.recv(SIZE).decode(FORMAT)
+    print(f"[KING CLIENT] se recibio el tamaño del archivo {filesize}")
+
+    for i in range(num_clients):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(ADDR)
+        client_sockets.append(client_socket)
+        print(f"[CLIENT {i}] Connected to server at {IP}:{PORT}")
+
+    # Recepción de mensajes
+    threads = []
+    for i in range(num_clients):
+        id_cliente = i
+        thread = threading.Thread(target=receive_messages, args=(client_sockets[i],archivo_transmision.decode(FORMAT),filesize,id_cliente,num_clients))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    client_socket_.sendall(DISCONNECT_MSG.encode(FORMAT))
+    client_socket_.close()
+if __name__ == "__main__":
+    main()
+    print(f"[CLIENT] HASH incorrectos: {hash_incorrecto}")
---
+++ TCP/server.py
@@ -0,0 +1,101 @@
+import socket
+import threading
+import hashlib
+import os
+import time 
+
+IP = "192.168.20.57"
+# IP = socket.gethostbyname(socket.gethostname())
+PORT = 5566
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!END"
+
+def handle_client(conn:socket, addr,filename,cantidad_clientes,id_cliente,f_):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    listo = conn.recv(SIZE).decode(FORMAT)
+    print(f"[READY][{addr}] {listo}")
+    if listo == "READY":
+        ALLready.append(conn)
+    while len(ALLready) < cantidad_clientes:
+        print(f"[READY][{addr}] Esperando a que todos los clientes esten listos")
+        pass
+
+    
+
+    
+    
+    tamaño = os.path.getsize(filename)
+    print(f"[ARCHIVO][{addr}] {filename} abierto")
+    # enviar el archivo por bloques de 1024 bytes
+    time_ini = time.time()
+    with open(filename, 'rb') as f:
+        offset = 0
+        while offset < tamaño:
+            # Leer el archivo en bloques de 1024 bytes
+            data = f.read(SIZE)
+            # Enviar el bloque al cliente
+            conn.sendall(data)
+            offset += len(data) 
+
+    fin = conn.recv(SIZE).decode(FORMAT)
+
+    if fin == "FIN":
+        time_fin = time.time()
+        time_dif = time_fin - time_ini
+        archivo = open(f"{filename}", "r")
+        print(f"[ARCHIVO][{addr}] {filename} enviado en {time_dif} segundos")
+        print(f"[ARCHIVO][{addr}] {filename} enviado")
+        hash_archivo = hashlib.md5(archivo.read().encode()).hexdigest()
+        print(f"[ARCHIVO][HASH][{addr}] {filename} leido")
+        conn.sendall(hash_archivo.encode(FORMAT))
+        print(f"[ARCHIVO][HASH][{addr}] {filename} enviado")
+        correcto = conn.recv(SIZE).decode(FORMAT)
+        archivo.close()
+        if correcto == "correcto":
+            f_.write(f"[CLIENTE][{id_cliente}][{addr}] {filename} recibido correctamente en {time_dif} segundos\n")
+        else:
+            f.write(f"[CLIENTE][{id_cliente}][{addr}] {filename} recibido incorrectamente en {time_dif} segundos\n")
+    conn.close()
+    
+    
+
+def main():
+    global ALLready
+    
+    print("[STARTING] Server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+    conexion_inicial, addr = server.accept()
+    print(f"[KING CONNECTION] {addr} connected.")
+    cantidad_clientes = int(conexion_inicial.recv(SIZE).decode(FORMAT))
+    print(f"[KING CONNECTION] se esperan {cantidad_clientes} clientes")
+    archivo_transmision = "Archivos/"+conexion_inicial.recv(SIZE).decode(FORMAT)+".txt"
+    print(f"[KING CONNECTION] espera el archivo {archivo_transmision}")
+    conexion_inicial.sendall(str(os.path.getsize(archivo_transmision)).encode(FORMAT))
+    ALLready = []
+    if not os.path.exists('Logs'):
+        os.makedirs('Logs')
+
+    f= open('Logs/'+time.strftime("%Y-%m-%d-%H-%M-%S")+'-log.txt', 'w') 
+    f.write(f"Archivo: {archivo_transmision} Tamaño: {os.path.getsize(archivo_transmision)} bytes\n")
+    f.write(f"Clientes: {cantidad_clientes}\n")
+    f.write(f"Tiempo de transferencia: \n")
+    for i in range(cantidad_clientes):
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr,archivo_transmision,cantidad_clientes,i,f))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+    if conexion_inicial.recv(SIZE).decode(FORMAT) == DISCONNECT_MSG:
+        print("[SERVER] Server is stopping...")
+        server.close()
+        print("[SERVER] Server stopped.")
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
