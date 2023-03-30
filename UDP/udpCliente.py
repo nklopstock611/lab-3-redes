4e499d75d109093dcd0adff5feec2313f2a87cbf
Brian <b.riverah@uniandes.edu.co>
2023-03-30 11:49:15 -0500

    fixed client

--- UDP/udpCliente.py
+++ UDP/udpCliente.py
@@ -12,8 +12,6 @@ server_address = ('192.168.20.60', 3400)
 def recive_data(sock, i, queue, numConnections):
 
     # Guardar en la carpeta ArchivosRecibidos
-    if not os.path.exists('UDP/ArchivosRecibidos'):
-        os.makedirs('UDP/ArchivosRecibidos')
     file = open("UDP/ArchivosRecibidos/Cliente" + str(i) + "-Prueba-" + numConnections + ".txt", "w")
     
     # Recibir respuesta
@@ -26,10 +24,11 @@ def recive_data(sock, i, queue, numConnections):
         if data == b'FIN':
             print(sys.stderr, 'Cliente ' + str(i) + ' - ' + 'Recibido "%s"' % data)
             break
+        
         file.write(data.decode())
 
     print(sys.stderr, 'Cliente ' + str(i) + ' - ' + 'Cerrando socket ' + str(i))
-
+    
     end_time = time()
     total_time = end_time - start_time
 
@@ -47,8 +46,7 @@ if __name__ == "__main__":
 
     # Obtener la fecha y hora actual para el nombre del archivo de logs
     actual_date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    if not os.path.exists('UDP/Logs'):
-        os.makedirs('UDP/Logs')
+
     log = open('UDP/Logs/' + actual_date + '-log.txt', 'w')        
     
     for i in range(0, int(sec_message)):
@@ -63,7 +61,7 @@ if __name__ == "__main__":
         # Enviar datos
         print(sys.stderr, 'Cliente ' + str(i) + ' - ' + 'Enviando') #  % message
         sent = sock.sendto(init_message, server_address)
-        
+        puerto_conexion = sock.recvfrom(4096).decode()
         queue = q.Queue()
 
         thread = threading.Thread(target=recive_data, args=(sock, i, queue, sec_message))
@@ -75,9 +73,10 @@ if __name__ == "__main__":
 
         # Verificación correctitud del archivo recibido
         filesize = os.path.getsize('UDP/ArchivosRecibidos/Cliente' + str(i) + '-Prueba-' + sec_message + '.txt')
-        puerto_conexion = sock.recvfrom(4096)
+        
         success = 'Error en transferencia (el archivo no se recibió completo)'
         if filesize == int(init_message.decode()) * 1048576:
             success = 'Transferencia exitosa'
-        log.write(f'[Cliente {i}][PuertoCliente {puerto_conexion}][{server_address[0]}:{server_address[1]}] Tiempo de transferencia: {total_time} s - {success}\n')
-    log.close()
+
+        log.write(f'[Cliente {i}][PuertoCliente {puerto_conexion}][{server_address[0]}:{server_address[1]}] Tiempo de transferencia: {total_time} s - {success} ({filesize} bytes vs {int(init_message.decode()) * 1048576}) velocidad de transferencia: {filesize / total_time} bytes/s \n')
+        
